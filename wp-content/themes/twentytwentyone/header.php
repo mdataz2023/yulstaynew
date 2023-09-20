74b3054f8855fb42bcb03fdefa03925eba462ffc
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-20 07:29:00 +0530

    test

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -10,7 +10,7 @@
  * @subpackage Twenty_Twenty_One
  * @since Twenty Twenty-One 1.0
  */
-
+    
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
