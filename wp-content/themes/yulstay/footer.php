7d5704e83b1b4a3ce995e9cb17b78f8af0462ee4
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-27 19:49:06 +0530

    H and F

--- wp-content/themes/yulstay/footer.php
+++ wp-content/themes/yulstay/footer.php
@@ -12,7 +12,147 @@
  */
 
 ?>
-			
+	        <div class="pxp-footer pt-100 pb-100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-lg-4">
+                        <div class="pxp-footer-logo">resideo.</div>
+                        <div class="pxp-footer-address mt-2">
+                            90 Fifth Avenue, 3rd Floor<br>
+                            San Francisco, CA 1980<br>
+                            (123) 456-7890
+                        </div>
+                        <div class="pxp-footer-social mt-2">
+                            <a href="#"><span class="fa fa-instagram"></span></a>
+                            <a href="#"><span class="fa fa-facebook-square"></span></a>
+                            <a href="#"><span class="fa fa-twitter"></span></a>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-lg-8">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-4">
+                                <h4 class="pxp-footer-header mt-4 mt-lg-0">Company</h4>
+                                <ul class="list-unstyled pxp-footer-links mt-2">
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Agents</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Demos</a></li>
+                                    <li><a href="#">Contact Us</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-12 col-md-4">
+                                <h4 class="pxp-footer-header mt-4 mt-lg-0">Actions</h4>
+                                <ul class="list-unstyled pxp-footer-links mt-2">
+                                    <li><a href="#">Buy Properties</a></li>
+                                    <li><a href="#">Rent Properties</a></li>
+                                    <li><a href="#">Sell Properties</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-12 col-md-4">
+                                <h4 class="pxp-footer-header mt-4 mt-lg-0">Explore</h4>
+                                <ul class="list-unstyled pxp-footer-links mt-2">
+                                    <li><a href="#">Homes for Rent</a></li>
+                                    <li><a href="#">Apartments for Rent</a></li>
+                                    <li><a href="#">Homes for Sale</a></li>
+                                    <li><a href="#">Apartments for Sale</a></li>
+                                    <li><a href="#">CRM</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="pxp-footer-bottom mt-2">
+                    <div><a href="#">Terms & Conditions</a> and <a href="#">Privacy Policy</a></div>
+                    <div class="pxp-footer-copyright">&copy; Resideo. All Rights Reserved. 2021</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="pxp-signin-modal" tabindex="-1" role="dialog" aria-labelledby="pxpSigninModal" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h5 class="modal-title" id="pxpSigninModal">Welcome back!</h5>
+                        <form class="mt-4">
+                            <div class="form-group">
+                                <label for="pxp-signin-email">Email</label>
+                                <input type="text" class="form-control" id="pxp-signin-email" placeholder="Enter your email address">
+                            </div>
+                            <div class="form-group">
+                                <label for="pxp-signin-pass">Password</label>
+                                <input type="password" class="form-control" id="pxp-signin-pass" placeholder="Enter your password">
+                            </div>
+                            <div class="form-group">
+                                <a href="#" class="pxp-agent-contact-modal-btn">Sign In</a>
+                            </div>
+                            <div class="form-group mt-4 text-center pxp-modal-small">
+                                <a href="#" class="pxp-modal-link">Forgot password</a>
+                            </div>
+                            <div class="text-center pxp-modal-small">
+                                New to Resideo? <a href="javascript:void(0);" class="pxp-modal-link pxp-signup-trigger">Create an account</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="pxp-signup-modal" tabindex="-1" role="dialog" aria-labelledby="pxpSignupModal" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h5 class="modal-title" id="pxpSignupModal">Create an account</h5>
+                        <form class="mt-4">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label for="pxp-signup-firstname">First Name</label>
+                                        <input type="text" class="form-control" id="pxp-signup-firstname" placeholder="Enter first name">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label for="pxp-signup-lastname">Last Name</label>
+                                        <input type="text" class="form-control" id="pxp-signup-lastname" placeholder="Enter last name">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="pxp-signup-email">Email</label>
+                                <input type="text" class="form-control" id="pxp-signup-email" placeholder="Enter your email address">
+                            </div>
+                            <div class="form-group">
+                                <label for="pxp-signup-pass">Password</label>
+                                <input type="password" class="form-control" id="pxp-signup-pass" placeholder="Create a password">
+                            </div>
+                            <div class="form-group">
+                                <a href="#" class="pxp-agent-contact-modal-btn">Sign Up</a>
+                            </div>
+                            <div class="text-center mt-4 pxp-modal-small">
+                                Already have an account? <a href="javascript:void(0);" class="pxp-modal-link pxp-signin-trigger">Sign in</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.4.1.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/popper.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/owl.carousel.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>//main.js?lasjkj"></script>		
 <?php wp_footer(); ?>
 
 </body>
--- wp-content/themes/yulstay/header.php
+++ wp-content/themes/yulstay/header.php
@@ -12,3 +12,88 @@
  */
 
 ?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700,900" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+
+        <title>resideo.</title>
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+        <div class="pxp-header fixed-top pxp-animate">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-5 col-md-2">
+                        <a href="index.html" class="pxp-logo text-decoration-none">resideo.</a>
+                    </div>
+                    <div class="col-2 col-md-8 text-center">
+                        <ul class="pxp-nav list-inline">
+                            <li class="list-inline-item">
+                                <a href="#">Home</a>
+                                <ul class="pxp-nav-sub rounded-lg">
+                                    <li><a href="index.html">Version 1</a></li>
+                                    <li><a href="index-2.html">Version 2</a></li>
+                                    <li><a href="index-3.html">Version 3</a></li>
+                                    <li><a href="index-4.html">Version 4</a></li>
+                                    <li><a href="index-5.html">Version 5</a></li>
+                                    <li><a href="index-5.html">Version 5</a></li>
+                                    <li><a href="index-6.html">Version 6</a></li>
+                                </ul>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Properties</a>
+                                <ul class="pxp-nav-sub rounded-lg">
+                                    <li><a href="properties.html">All Properties</a></li>
+                                    <li><a href="single-property.html">Single Property</a></li>
+                                    <li><a href="submit-property.html">Submit New Property</a></li>
+                                </ul>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Agents</a>
+                                <ul class="pxp-nav-sub rounded-lg">
+                                    <li><a href="agents.html">All Agents</a></li>
+                                    <li><a href="single-agent.html">Single Agent</a></li>
+                                </ul>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Blog</a>
+                                <ul class="pxp-nav-sub rounded-lg">
+                                    <li><a href="blog.html">All Blog Posts</a></li>
+                                    <li><a href="single-post.html">Single Blog Post</a></li>
+                                </ul>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#">Themes</a>
+                                <ul class="pxp-nav-sub rounded-lg">
+                                    <li><a href="../light/index-2.html">Light</a></li>
+                                    <li><a href="../dark/index-2.html">Dark</a></li>
+                                </ul>
+                            </li>
+                            <li class="list-inline-item pxp-is-last"><a href="contact.html">Contact Us</a></li>
+                            <li class="list-inline-item pxp-has-btns">
+                                <div class="pxp-user-btns">
+                                    <a href="#" class="pxp-user-btns-signup pxp-signup-trigger">Sign Up</a>
+                                    <a href="#" class="pxp-user-btns-login pxp-signin-trigger">Sign In</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-5 col-md-2 text-right">
+                        <a href="javascript:void(0);" class="pxp-header-nav-trigger"><span class="fa fa-bars"></span></a>
+                        <a href="javascript:void(0);" class="pxp-header-user pxp-signin-trigger"><span class="fa fa-user-o"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
