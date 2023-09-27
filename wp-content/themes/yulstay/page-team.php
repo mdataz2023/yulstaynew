276895c49acc526165843eb7ba99ff4b1b777012
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-27 21:35:29 +0530

    team page

--- wp-content/themes/yulstay/page-team.php
+++ wp-content/themes/yulstay/page-team.php
@@ -55,21 +55,37 @@ get_header();?>
 
                 <div class="container">
                     <div class="row mt-200">
+                    <?php $the_query = new WP_Query( array('post_type' =>'team','posts_per_page' => '20',  'post__not_in'   => array( $id),) );?>
+							     <?php if ( have_posts())   : while ( $the_query->have_posts() ) : $the_query->the_post();?>
+							  
+									   <?php
+										$thumbnail_id = get_post_thumbnail_id(); 
+										$thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true );
+										$thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true); 
+
+                    $categories = get_the_category();
+										
+										?>
                         <div class="col-sm-12 col-md-6 col-lg-3">
-                            <a href="single-agent.html" class="pxp-agents-1-item">
+                            <a href="<?php the_permalink(); ?>" class="pxp-agents-1-item">
+                            <?php if ( has_post_thumbnail() ) {
+                                $attachment_image = wp_get_attachment_url( get_post_thumbnail_id() );
+                                //echo '<link rel="preload" as="image" href="' . esc_attr( $attachment_image ) . '">';  
+                            ?>
                                 <div class="pxp-agents-1-item-fig-container rounded-lg">
-                                    <div class="pxp-agents-1-item-fig pxp-cover" style="background-image: url(images/ph-agent.jpg); background-position: top center"></div>
+                                    <div class="pxp-agents-1-item-fig pxp-cover" style="background-image: url(<?php echo $attachment_image; ?>); background-position: top center"></div>
                                 </div>
+                                <?php } ?>
                                 <div class="pxp-agents-1-item-details rounded-lg">
-                                    <div class="pxp-agents-1-item-details-name">Scott Goodwin</div>
+                                    <div class="pxp-agents-1-item-details-name"><?php the_title(); ?></div>
                                     <div class="pxp-agents-1-item-details-email"><span class="fa fa-phone"></span> (123) 456-7890</div>
                                     <div class="pxp-agents-1-item-details-spacer"></div>
-                                    <div class="pxp-agents-1-item-cta text-uppercase">More Details</div>
+                                    <a href="<?php the_permalink(); ?>"><div class="pxp-agents-1-item-cta text-uppercase">More Details</div></a>
                                 </div>
                                 <div class="pxp-agents-1-item-rating"><span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></span></div>
                             </a>
                         </div>
-                        <div class="col-sm-12 col-md-6 col-lg-3">
+                        <!-- <div class="col-sm-12 col-md-6 col-lg-3">
                             <a href="single-agent.html" class="pxp-agents-1-item">
                                 <div class="pxp-agents-1-item-fig-container rounded-lg">
                                     <div class="pxp-agents-1-item-fig pxp-cover" style="background-image: url(images/ph-agent.jpg); background-position: top center"></div>
@@ -166,7 +182,7 @@ get_header();?>
                                 </div>
                                 <div class="pxp-agents-1-item-rating"><span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star-o"></span></span></div>
                             </a>
-                        </div>
+                        </div> -->
                     </div>
         
                     <ul class="pagination pxp-paginantion mt-2 mt-md-3">
