c32d1d4c9b8e6281f2d253c242dc6abc464ee93c
Asanka Tharindu <asankatharindu1111@.com>
2023-09-28 00:10:24 +0530

    acf fild add to single-team

--- wp-content/themes/yulstay/single-team.php
+++ wp-content/themes/yulstay/single-team.php
@@ -20,7 +20,7 @@ $team_member_telephone=get_field('team_member_telephone');
                             <p><?php echo $team_member_destination; ?></p>
                             <div class="mt-4 mt-md-5">
                                 <div class="pxp-agent-email"><a href="mailto:erika.tillman@resideo.com"><span class="fa fa-envelope-o"></span> <?php echo $team_member_email; ?></a></div>
-                                <div class="pxp-agent-phone"><span class="fa fa-phone"></span> <?php echo $team_member_telephone; ?></div>
+                                <div class="pxp-agent-phone"><span class="fa fa-phone"></span> <p><?php echo $team_member_telephone; ?></p></div>
                             </div>
                             <div class="mt-4 mt-md-5">
                                 <a href="#pxp-work-with" class="pxp-agent-contact-btn" data-toggle="modal" data-target="#pxp-work-with">Work with <?php the_title(); ?></a>
