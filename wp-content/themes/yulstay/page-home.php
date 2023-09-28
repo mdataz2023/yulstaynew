067f0681dd2e49cbbc18f59bdff93ebbdb266054
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-28 20:25:18 +0530

    w

--- wp-content/themes/yulstay/page-home.php
+++ wp-content/themes/yulstay/page-home.php
@@ -237,26 +237,6 @@
                                 <div class="pxp-services-item-text-sub">Find an agent who knows<br>your market best</div>
                             </div>
                             <div class="pxp-services-item-cta text-uppercase text-center">Learn More</div>
-                        </a>
-                        <a href="properties.html" class="pxp-services-item">
-                            <div class="pxp-services-item-fig">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-3.svg" alt="...">
-                            </div>
-                            <div class="pxp-services-item-text text-center">
-                                <div class="pxp-services-item-text-title">Buy or rent homes</div>
-                                <div class="pxp-services-item-text-sub">Millions of houses and apartments in<br>your favourite cities</div>
-                            </div>
-                            <div class="pxp-services-item-cta text-uppercase text-center">Learn More</div>
-                        </a>
-                        <a href="submit-property.html" class="pxp-services-item">
-                            <div class="pxp-services-item-fig">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-4.svg" alt="...">
-                            </div>
-                            <div class="pxp-services-item-text text-center">
-                                <div class="pxp-services-item-text-title">List your own property</div>
-                                <div class="pxp-services-item-text-sub">Sign up now and sell or rent<br>your own properties</div>
-                            </div>
-                            <div class="pxp-services-item-cta text-uppercase text-center">Learn More</div>
                         </a> -->
                         <div class="clearfix"></div>
                     </div>
