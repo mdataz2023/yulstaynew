700dce1b96562c6dffa272f28d36cb1e71dfe211
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-28 10:10:59 +0530

    Test

--- wp-content/themes/yulstay/page-home.php
+++ wp-content/themes/yulstay/page-home.php
@@ -484,11 +484,6 @@
                 </div>
             </div>
         </div>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.4.1.min.js"></script>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/popper.min.js"></script>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/owl.carousel.min.js"></script>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/main.js?lasjkj"></script>
 
 
 <?php get_footer(); ?>
