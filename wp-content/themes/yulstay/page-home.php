008fc74c50ee79f6101ba828e15ef02f0a89cd14
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-28 13:23:50 +0530

    Test

--- wp-content/themes/yulstay/page-home.php
+++ wp-content/themes/yulstay/page-home.php
@@ -129,17 +129,33 @@
             </div> -->
 
             <div class="container mt-100">
-                <h2 class="pxp-section-h2">Our Featured Agents</h2>
+                <h2 class="pxp-section-h2">Our Featured Team Members</h2>
                 <p class="pxp-text-light">Meet the best real estate agents</p>
 
                 <div class="row mt-4 mt-md-5">
+                <?php $the_query = new WP_Query( array('post_type' =>'team','posts_per_page' => '4',  'post__not_in'   => array( $id),) );?>
+							     <?php if ( have_posts())   : while ( $the_query->have_posts() ) : $the_query->the_post();?>
+							  
+									   <?php
+										$thumbnail_id = get_post_thumbnail_id(); 
+										$thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true );
+										$thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true); 
+
+                    $categories = get_the_category();
+										
+										?> 
                     <div class="col-sm-12 col-md-6 col-lg-3">
-                        <a href="single-agent.html" class="pxp-agents-1-item">
+                        <a href="<?php the_permalink(); ?>" class="pxp-agents-1-item">
+                        <?php if ( has_post_thumbnail() ) {
+                                $attachment_image = wp_get_attachment_url( get_post_thumbnail_id() );
+                                //echo '<link rel="preload" as="image" href="' . esc_attr( $attachment_image ) . '">';  
+                            ?>
                             <div class="pxp-agents-1-item-fig-container rounded-lg">
-                                <div class="pxp-agents-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-agent.jpg); background-position: top center"></div>
+                                <div class="pxp-agents-1-item-fig pxp-cover" style="background-image: url(<?php echo $attachment_image; ?>); background-position: top center"></div>
                             </div>
+                            <?php } ?>
                             <div class="pxp-agents-1-item-details rounded-lg">
-                                <div class="pxp-agents-1-item-details-name">Scott Goodwin</div>
+                                <div class="pxp-agents-1-item-details-name"><?php the_title(); ?></div>
                                 <div class="pxp-agents-1-item-details-email"><span class="fa fa-phone"></span> (123) 456-7890</div>
                                 <div class="pxp-agents-1-item-details-spacer"></div>
                                 <div class="pxp-agents-1-item-cta text-uppercase">More Details</div>
@@ -147,7 +163,8 @@
                             <div class="pxp-agents-1-item-rating"><span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></span></div>
                         </a>
                     </div>
-                    <div class="col-sm-12 col-md-6 col-lg-3">
+                    <?php endwhile; endif; ?>
+                    <!-- <div class="col-sm-12 col-md-6 col-lg-3">
                         <a href="single-agent.html" class="pxp-agents-1-item">
                             <div class="pxp-agents-1-item-fig-container rounded-lg">
                                 <div class="pxp-agents-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-agent.jpg); background-position: top center"></div>
@@ -160,35 +177,7 @@
                             </div>
                             <div class="pxp-agents-1-item-rating"><span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star-o"></span></span></div>
                         </a>
-                    </div>
-                    <div class="col-sm-12 col-md-6 col-lg-3">
-                        <a href="single-agent.html" class="pxp-agents-1-item">
-                            <div class="pxp-agents-1-item-fig-container rounded-lg">
-                                <div class="pxp-agents-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-agent.jpg); background-position: top center"></div>
-                            </div>
-                            <div class="pxp-agents-1-item-details rounded-lg">
-                                <div class="pxp-agents-1-item-details-name">Melvin Blackwell</div>
-                                <div class="pxp-agents-1-item-details-email"><span class="fa fa-phone"></span> (789) 123-4560</div>
-                                <div class="pxp-agents-1-item-details-spacer"></div>
-                                <div class="pxp-agents-1-item-cta text-uppercase">More Details</div>
-                            </div>
-                            <div class="pxp-agents-1-item-rating"><span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></span></div>
-                        </a>
-                    </div>
-                    <div class="col-sm-12 col-md-6 col-lg-3">
-                        <a href="single-agent.html" class="pxp-agents-1-item">
-                            <div class="pxp-agents-1-item-fig-container rounded-lg">
-                                <div class="pxp-agents-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-agent.jpg); background-position: top center"></div>
-                            </div>
-                            <div class="pxp-agents-1-item-details rounded-lg">
-                                <div class="pxp-agents-1-item-details-name">Erika Tillman</div>
-                                <div class="pxp-agents-1-item-details-email"><span class="fa fa-phone"></span> (890) 456-1237</div>
-                                <div class="pxp-agents-1-item-details-spacer"></div>
-                                <div class="pxp-agents-1-item-cta text-uppercase">More Details</div>
-                            </div>
-                            <div class="pxp-agents-1-item-rating"><span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star-o"></span></span></div>
-                        </a>
-                    </div>
+                    </div> -->
                 </div>
 
                 <a href="agents.html" class="pxp-primary-cta text-uppercase mt-1 mt-md-4 pxp-animate">See All Agents</a>
@@ -423,66 +412,6 @@
                 <a href="properties.html" class="pxp-primary-cta text-uppercase mt-2 mt-md-4 pxp-animate">Explore Neighborhoods</a>
             </div>
 
-            <div class="container mt-100">
-                <h2 class="pxp-section-h2 text-center">Membership Plans</h2>
-                <p class="pxp-text-light text-center">Choose the plan that suits you best</p>
-                <div class="row mt-5">
-                    <div class="col-sm-12 col-md-6 col-lg-4">
-                        <a href="#" class="pxp-plans-1-item">
-                            <div class="pxp-plans-1-item-fig">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/plan-personal.svg" alt="...">
-                            </div>
-                            <div class="pxp-plans-1-item-title">Personal</div>
-                            <ul class="pxp-plans-1-item-features list-unstyled">
-                                <li>10 Listings</li>
-                                <li>2 Featured Listings</li>
-                            </ul>
-                            <div class="pxp-plans-1-item-price">
-                                <span class="pxp-plans-1-item-price-sum">Free</span>
-                                <span class="pxp-plans-1-item-price-period"> / 1 month</span>
-                            </div>
-                            <div class="pxp-plans-1-item-cta text-uppercase">Choose Plan</div>
-                        </a>
-                    </div>
-                    <div class="col-sm-12 col-md-6 col-lg-4">
-                        <a href="#" class="pxp-plans-1-item pxp-is-popular">
-                            <div class="pxp-plans-1-item-label">Most Popular</div>
-                            <div class="pxp-plans-1-item-fig">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/plan-professional.svg" alt="...">
-                            </div>
-                            <div class="pxp-plans-1-item-title">Professional</div>
-                            <ul class="pxp-plans-1-item-features list-unstyled">
-                                <li>10 Listings</li>
-                                <li>2 Featured Listings</li>
-                            </ul>
-                            <div class="pxp-plans-1-item-price">
-                                <span class="pxp-plans-1-item-price-currency">$</span>
-                                <span class="pxp-plans-1-item-price-sum">49.99</span>
-                                <span class="pxp-plans-1-item-price-period"> / 6 months</span>
-                            </div>
-                            <div class="pxp-plans-1-item-cta text-uppercase">Choose Plan</div>
-                        </a>
-                    </div>
-                    <div class="col-sm-12 col-md-6 col-lg-4">
-                        <a href="#" class="pxp-plans-1-item">
-                            <div class="pxp-plans-1-item-fig">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/plan-business.svg" alt="...">
-                            </div>
-                            <div class="pxp-plans-1-item-title">Business</div>
-                            <ul class="pxp-plans-1-item-features list-unstyled">
-                                <li>10 Listings</li>
-                                <li>2 Featured Listings</li>
-                            </ul>
-                            <div class="pxp-plans-1-item-price">
-                                <span class="pxp-plans-1-item-price-currency">$</span>
-                                <span class="pxp-plans-1-item-price-sum">99.99</span>
-                                <span class="pxp-plans-1-item-price-period"> / 1 year</span>
-                            </div>
-                            <div class="pxp-plans-1-item-cta text-uppercase">Choose Plan</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
         </div>
 
 
