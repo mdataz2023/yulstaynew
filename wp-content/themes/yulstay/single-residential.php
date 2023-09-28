efe3b0e71d4662b3b299f70ecfeb3a0e03eee214
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-28 14:02:02 +0530

    Test

--- wp-content/themes/yulstay/single-residential.php
+++ wp-content/themes/yulstay/single-residential.php
@@ -374,4 +374,17 @@ get_header();
             </div>
         </div>
 
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.4.1.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/popper.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_KEY_HERE&amp;libraries=geometry&amp;libraries=places"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/photoswipe.min.js"></script> 
+        <script src="<?php echo get_template_directory_uri(); ?>/js/photoswipe-ui-default.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.sticky.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/gallery.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/infobox.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/single-map.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/Chart.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/main.js?asdasd12"></script>
+
 <?php get_footer(); ?>
