8002174bcf3bf725d71f3edbbc3791af10814e93
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-29 23:47:32 +0530

    f

--- wp-content/themes/yulstay/page-home.php
+++ wp-content/themes/yulstay/page-home.php
@@ -100,7 +100,7 @@
             <div class="pxp-hero vh-100">
                 <div class="pxp-hero-bg pxp-cover pxp-cover-bottom">
                     <video autoplay loop muted playsinline>
-                        <source src="<?php echo get_template_directory_uri(); ?>/images/montreal-city.m4v" type="video/webm">
+                        <source src="<?php echo get_template_directory_uri(); ?>/images/montreal-city.webm" type="video/webm">
                         <!-- <source src="<?php echo get_template_directory_uri(); ?>/images/Yulstay.mp4" type="video/mp4"> -->
                         <!-- You can add additional source elements for different video formats (e.g., WebM, Ogg) -->
                         Your browser does not support the video tag.
