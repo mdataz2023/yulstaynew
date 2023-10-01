0dc2128ae8eb9fe6989c066433fa04d1f50afd9c
dulanchandrakumara <140385146+dulanchandrakumara@users.noreply.github.com>
2023-10-01 13:19:05 +0530

    function

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -8586,3 +8586,25 @@ function is_php_version_compatible( $required ) {
 function wp_fuzzy_number_match( $expected, $actual, $precision = 1 ) {
 	return abs( (float) $expected - (float) $actual ) <= $precision;
 }
+
+
+/**
+ * schedule_post_publish_event runs when a Post is Published
+ */
+add_action( 'publish_post', 'schedule_post_publish_event' );
+/**
+ * when user posts this function will call
+ */
+function schedule_post_publish_event( $post_id ) {
+    // Schedule the actual event
+    wp_schedule_single_event( 1 * MINUTE_IN_SECONDS, 'post_publish_send_email');
+}
+/**
+ * this method will call when cron executes
+ */
+function post_publish_send_email() {
+    //init time
+    $time = date("Y-m-d h:i:sa", time());
+    //send email to admin
+    wp_mail( 'dulan.imovil@gmail..com', 'New post has been published', 'New post has been published on' . $time );
+}
