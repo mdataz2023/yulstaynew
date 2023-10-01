0fee2039b97f64a454d262d234a32ade0d2cf3e8
dulanchandrakumara <140385146+dulanchandrakumara@users.noreply.github.com>
2023-10-01 13:25:50 +0530

    function

--- wp-content/themes/yulstay/functions.php
+++ wp-content/themes/yulstay/functions.php
@@ -14,4 +14,27 @@ function register_main_lang() {
     register_nav_menu('main-lang',__( 'Main lang' ));
 }
 add_action( 'init', 'register_main_lang' );
+
+
+/**
+ * schedule_post_publish_event runs when a Post is Published
+ */
+add_action( 'publish_post', 'schedule_post_publish_event' );
+
+/**
+ * when user posts this function will call
+ */
+function schedule_post_publish_event( $post_id ) {
+    // Schedule the actual event
+    wp_schedule_single_event( 1 * MINUTE_IN_SECONDS, 'post_publish_send_email');
+}
+/**
+ * this method will call when cron executes
+ */
+function post_publish_send_email() {
+    //init time
+    $time = date("Y-m-d h:i:sa", time());
+    //send email to admin
+    wp_mail( 'dulan.imovil@gmail..com', 'New post has been published', 'New post has been published on' . $time );
+}
 ?>
