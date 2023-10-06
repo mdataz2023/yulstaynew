3632dadedd090a2c34023275447060054cb3f0e4
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-10-06 10:18:37 +0530

    Test

--- wp-content/themes/yulstay/page-db.php
+++ wp-content/themes/yulstay/page-db.php
@@ -9,30 +9,17 @@ get_header();
 <div>
 <?php
 global $wpdb;
-$table_name = $wpdb->prefix . 'REGIONS'; 
-$sql = "SELECT * FROM $table_name";
-$table_data = $wpdb->get_results($sql);
 
-echo '<table>';
-echo '<thead>';
-echo '<tr>';
-echo '<th>Column 1</th>';
-echo '<th>Column 2</th>';
-echo '<th>Column 2</th>';
-echo '</tr>';
-echo '</thead>';
-echo '<tbody>';
+// Perform a simple query to check the database connection
+$result = $wpdb->get_var("SELECT 1");
 
-foreach ($table_data as $row) {
-    echo '<tr>';
-    echo '<td>' . esc_html($row->CODE) . '</td>';
-    echo '<td>' . esc_html($row->DESCRIPTION_FRANCAISE) . '</td>';
-    echo '<td>' . esc_html($row->DESCRIPTION_ANGLAISE) . '</td>';
-    echo '</tr>';
+if ($wpdb->last_error) {
+    // If there is an error, print or log it
+    echo "Database connection error: " . $wpdb->last_error;
+} else {
+    // Database connection is successful
+    echo "Database connection is working!";
 }
-
-echo '</tbody>';
-echo '</table>';
 ?>
 </div>
 
