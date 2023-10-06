c389e2d83b87a185ab41e15a1be132969d62fd23
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-10-06 09:58:18 +0530

    Test

--- wp-content/themes/yulstay/page-db.php
+++ wp-content/themes/yulstay/page-db.php
@@ -1,42 +1,34 @@
 <?php
 /*
-	Template Name: DB
+    Template Name: DB
 */
 get_header();
 
-
 global $wpdb;
-$table_name = $wpdb->prefix . 'PHOTOS'; 
+$table_name = $wpdb->prefix . 'REGIONS'; 
 $sql = "SELECT * FROM $table_name";
 $table_data = $wpdb->get_results($sql);
 
-if (!empty($table_data)) {
-    echo '<table>';
-    echo '<thead>';
+echo '<table>';
+echo '<thead>';
+echo '<tr>';
+echo '<th>Column 1</th>';
+echo '<th>Column 2</th>';
+echo '<th>Column 2</th>';
+echo '</tr>';
+echo '</thead>';
+echo '<tbody>';
+
+foreach ($table_data as $row) {
     echo '<tr>';
-    echo '<th>Column 1</th>';
-    echo '<th>Column 2</th>';
-    // Add more table headers as needed
+    echo '<td>' . esc_html($row->CODE) . '</td>';
+    echo '<td>' . esc_html($row->DESCRIPTION_FRANCAISE) . '</td>';
+    echo '<td>' . esc_html($row->DESCRIPTION_ANGLAISE) . '</td>';
     echo '</tr>';
-    echo '</thead>';
-    echo '<tbody>';
-
-    foreach ($table_data as $row) {
-        echo '<tr>';
-        echo '<td>' . esc_html($row->column1) . '</td>';
-        echo '<td>' . esc_html($row->column2) . '</td>';
-        // Add more table columns as needed
-        echo '</tr>';
-    }
-
-    echo '</tbody>';
-    echo '</table>';
-} else {
-    echo 'No data found in the table.';
 }
 
+echo '</tbody>';
+echo '</table>';
+
+get_footer();
 ?>
-
-
-
-<?php get_footer(); ?>
