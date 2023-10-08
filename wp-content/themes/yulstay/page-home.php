5253b801c263dd13cf959dcf8192ab66de857d04
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-10-08 19:28:03 +0530

    Home lang

--- wp-content/themes/yulstay/page-home.php
+++ wp-content/themes/yulstay/page-home.php
@@ -755,9 +755,19 @@ unlink($zipFolder."INSCRIPTIONS.TXT");
                     </ul>
                 </div>
                 <div class="col-5 col-md-2 text-right">
-                    <a href="javascript:void(0);" class="pxp-header-nav-trigger"><span class="fa fa-bars"></span></a>
+                    <!-- <a href="javascript:void(0);" class="pxp-header-nav-trigger"><span class="fa fa-bars"></span></a>
                     <a href="javascript:void(0);" class="pxp-header-user pxp-signin-trigger"><span
-                            class="fa fa-user-o"></span></a>
+                            class="fa fa-user-o"></span></a> -->
+                            <?php
+                      if (has_nav_menu('main-lang')) {
+                          wp_nav_menu(
+                              array(
+                                  'theme_location' => 'main-lang',
+                                  'menu_class'     => 'main-lang', 
+                              )
+                          );
+                      }
+                      ?>
                 </div>
             </div>
         </div>
