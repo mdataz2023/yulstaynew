ffc7ee2d3e6b0804c9a2579153400b49d08831f5
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-10-09 00:12:50 +0530

    Test

--- wp-content/themes/yulstay/page-home.php
+++ wp-content/themes/yulstay/page-home.php
@@ -917,52 +917,46 @@ unlink($zipFolder."INSCRIPTIONS.TXT");
             <h2 class="text-center pxp-section-h2"><?php echo $home_why_choose_us_section_title; ?></h2>
             <p class="pxp-text-light text-center"><?php echo $home_why_choose_us_section_description; ?></p>
 
-            <?php
-                $why_choose_us_card_one_link = get_field('why_choose_us_card_one_link');
-                $why_choose_us_card_one_title = get_field('why_choose_us_card_one_title');
-                $why_choose_us_card_one_description = get_field('why_choose_us_card_one_description');
-                $why_choose_us_card_one_image = get_field('why_choose_us_card_one_image');
-            ?>
 
             <div class="container">
                 <div class="pxp-services-container rounded-lg mt-4 mt-md-5">
-                        <a href="<?php echo $why_choose_us_card_one_link; ?>" class="pxp-services-item">
-                            <div class="pxp-services-item-fig" style="color:#00aec5;">
-                                <?php echo $why_choose_us_card_one_image; ?>
-                            </div>
-                            <div class="pxp-services-item-text text-center">
-                                <div class="pxp-services-item-text-title"><?php echo $why_choose_us_card_one_title; ?></div>
-                                <div class="pxp-services-item-text-sub"><?php echo $why_choose_us_card_one_description; ?></div>
-                            </div>
-                            <div class="pxp-services-item-cta text-uppercase text-center">Learn More</div>
-                        </a>
-                        <a href="<?php echo get_field('why_choose_us_card_two_link'); ?>" class="pxp-services-item">
+                <a href="properties.html" class="pxp-services-item">
                             <div class="pxp-services-item-fig">
-                                <img src="<?php echo get_field('why_choose_us_card_two_image'); ?>" alt="...">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-1.svg" alt="...">
                             </div>
                             <div class="pxp-services-item-text text-center">
-                                <div class="pxp-services-item-text-title"><?php echo get_field('why_choose_us_card_two_title'); ?></div>
-                                <div class="pxp-services-item-text-sub"><?php echo get_field('why_choose_us_card_two_description'); ?></div>
+                                <div class="pxp-services-item-text-title">Find your future home</div>
+                                <div class="pxp-services-item-text-sub">We help you find a new home by offering<br>a smart real estate experience</div>
                             </div>
                             <div class="pxp-services-item-cta text-uppercase text-center">Learn More</div>
                         </a>
-                        <a href="<?php echo get_field('why_choose_us_card_three_link'); ?>" class="pxp-services-item">
+                        <a href="agents.html" class="pxp-services-item">
                             <div class="pxp-services-item-fig">
-                                <img src="<?php echo get_field('why_choose_us_card_three_image'); ?>" alt="...">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-2.svg" alt="...">
                             </div>
                             <div class="pxp-services-item-text text-center">
-                                <div class="pxp-services-item-text-title"><?php echo get_field('why_choose_us_card_three_title'); ?></div>
-                                <div class="pxp-services-item-text-sub"><?php echo get_field('why_choose_us_card_three_description'); ?></div>
+                                <div class="pxp-services-item-text-title">Experienced agents</div>
+                                <div class="pxp-services-item-text-sub">Find an agent who knows<br>your market best</div>
                             </div>
                             <div class="pxp-services-item-cta text-uppercase text-center">Learn More</div>
                         </a>
-                        <a href="<?php echo get_field('why_choose_us_card_four_link'); ?>" class="pxp-services-item">
-                            <div class="pxp-services-item-fig" style="color:#00aec5;">
-                                <img src="<?php echo get_field('why_choose_us_card_four_image'); ?>" alt="...">
+                        <a href="properties.html" class="pxp-services-item">
+                            <div class="pxp-services-item-fig">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-3.svg" alt="...">
                             </div>
                             <div class="pxp-services-item-text text-center">
-                                <div class="pxp-services-item-text-title"><?php echo get_field('why_choose_us_card_four_title'); ?></div>
-                                <div class="pxp-services-item-text-sub"><?php echo get_field('why_choose_us_card_four_description'); ?></div>
+                                <div class="pxp-services-item-text-title">Buy or rent homes</div>
+                                <div class="pxp-services-item-text-sub">Millions of houses and apartments in<br>your favourite cities</div>
+                            </div>
+                            <div class="pxp-services-item-cta text-uppercase text-center">Learn More</div>
+                        </a>
+                        <a href="submit-property.html" class="pxp-services-item">
+                            <div class="pxp-services-item-fig">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-4.svg" alt="...">
+                            </div>
+                            <div class="pxp-services-item-text text-center">
+                                <div class="pxp-services-item-text-title">List your own property</div>
+                                <div class="pxp-services-item-text-sub">Sign up now and sell or rent<br>your own properties</div>
                             </div>
                             <div class="pxp-services-item-cta text-uppercase text-center">Learn More</div>
                         </a>
