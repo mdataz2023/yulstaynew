160959e1f7ed03cfaf8a4320c414b13dea0455a9
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-10-18 16:20:19 +0530

    Test

---
+++ wp-content/themes/yulstay/page-residential-info.php
@@ -0,0 +1,132 @@
+<?php
+/*
+	Template Name: Residential Info
+*/
+get_header();
+
+?>
+
+        <div class="pxp-content">
+            <div class="pxp-hero vh-100" style="padding-top: 80px;">
+                <div class="pxp-hero-props-carousel-2">
+                    <div class="carousel slide pxp-hero-props-carousel-2-left" data-ride="carousel" data-pause="false" data-interval="false">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-slide="0" style="background-color: #D4AA32;">
+                                <div class="pxp-caption">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-10">
+                                            <div class="pxp-caption-prop-title">SALE</div>
+                                        </div>
+                                    </div>
+                                    <!-- <div class="pxp-caption-prop-features mt-4">2 BD <span>|</span> 2 BA <span>|</span> 920 SF</div>
+                                    <div class="pxp-caption-prop-price mt-5">$890,000</div> -->
+                                </div>
+                                <div>
+                                    <a href="single-property.html" class="pxp-cta text-uppercase pxp-animate">Residential Team</a>
+                                </div>
+                            </div>
+                            <div class="carousel-item" data-slide="1" style="background-color: #64827C;">
+                                <div class="pxp-caption">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-10">
+                                            <div class="pxp-caption-prop-title">PURCHASE</div>
+                                        </div>
+                                    </div>
+                                    <!-- <div class="pxp-caption-prop-features mt-4">1 BD <span>|</span> 1 BA <span>|</span> 500 SF</div>
+                                    <div class="pxp-caption-prop-price mt-5">$2,675</div> -->
+                                </div>
+                                <a href="single-property.html" class="pxp-cta text-uppercase pxp-animate">Residential Team</a>
+                            </div>
+                            <div class="carousel-item" data-slide="2" style="background-color: #687389;">
+                                <div class="pxp-caption">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-10">
+                                            <div class="pxp-caption-prop-title">INVESTMENT</div>
+                                        </div>
+                                    </div>
+                                    <!-- <div class="pxp-caption-prop-features mt-4">2 BD <span>|</span> 2 BA <span>|</span> 870 SF</div>
+                                    <div class="pxp-caption-prop-price mt-5">$960,000</div> -->
+                                </div>
+                                <a href="single-property.html" class="pxp-cta text-uppercase pxp-animate">Residential Team</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="pxp-hero-props-carousel-2-right" class="carousel slide pxp-hero-props-carousel-2-right" data-ride="carousel" data-pause="false" data-interval="7000">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-slide="0">
+                                <div class="pxp-hero-bg pxp-cover" style="background-image: url(images/ph-big.jpg); background-position: 0% 50%;">
+                                    <div style="display: flex; flex-direction: column; padding: 100px;">
+                                        <p style="font-size: 14px;">It is imperative to understand the current market condition and draft a proper strategy tailored to you and your property. The goal is always to increase visibility and generate a high level of interest in order to maximize value.</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Our mission is simple </h3>
+                                        <p style="font-size: 14px;">Help our clients sell their property with complete peace of mind by offering result-oriented strategies and services with the highest degree of transparency. We hold true to our mission and are able to deliver on it by focusing on our three core elements</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Peace of mind </h3>
+                                        <p style="font-size: 14px;">Our team has an 18 + 1 step plan of action that has been proven to alleviate you from all the stress associated with the sale of your property.</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Transparency</h3>
+                                        <p style="font-size: 14px;">We take the time to properly explain the current market conditions and what our step-by-step strategy to sell your property will be. We pride ourselves in maintaining a high level of communication throughout the entire process to ensure that all your questions and concerns are addressed.</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Results</h3>
+                                        <p style="font-size: 14px;">Our tailored made strategies have a proven track record showing that we continuously deliver on our promise to maximize your returns.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item" data-slide="1">
+                                <div class="pxp-hero-bg pxp-cover" style="background-image: url(images/ph-big.jpg); background-position: 50% 50%;">
+                                    <div style="display: flex; flex-direction: column; padding: 100px;">
+                                        <p style="font-size: 14px;">It is imperative to understand the current market condition and draft a proper strategy tailored to you and your property. The goal is always to increase visibility and generate a high level of interest in order to maximize value.</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Our mission is simple </h3>
+                                        <p style="font-size: 14px;">Help our clients sell their property with complete peace of mind by offering result-oriented strategies and services with the highest degree of transparency. We hold true to our mission and are able to deliver on it by focusing on our three core elements</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Peace of mind </h3>
+                                        <p style="font-size: 14px;">Our team has an 18 + 1 step plan of action that has been proven to alleviate you from all the stress associated with the sale of your property.</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Transparency</h3>
+                                        <p style="font-size: 14px;">We take the time to properly explain the current market conditions and what our step-by-step strategy to sell your property will be. We pride ourselves in maintaining a high level of communication throughout the entire process to ensure that all your questions and concerns are addressed.</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Results</h3>
+                                        <p style="font-size: 14px;">Our tailored made strategies have a proven track record showing that we continuously deliver on our promise to maximize your returns.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item" data-slide="2">
+                                <div class="pxp-hero-bg pxp-cover" style="background-image: url(images/ph-big.jpg); background-position: 50% 50%;">
+                                    <div style="display: flex; flex-direction: column; padding: 100px;">
+                                        <p style="font-size: 14px;">It is imperative to understand the current market condition and draft a proper strategy tailored to you and your property. The goal is always to increase visibility and generate a high level of interest in order to maximize value.</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Our mission is simple </h3>
+                                        <p style="font-size: 14px;">Help our clients sell their property with complete peace of mind by offering result-oriented strategies and services with the highest degree of transparency. We hold true to our mission and are able to deliver on it by focusing on our three core elements</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Peace of mind </h3>
+                                        <p style="font-size: 14px;">Our team has an 18 + 1 step plan of action that has been proven to alleviate you from all the stress associated with the sale of your property.</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Transparency</h3>
+                                        <p style="font-size: 14px;">We take the time to properly explain the current market conditions and what our step-by-step strategy to sell your property will be. We pride ourselves in maintaining a high level of communication throughout the entire process to ensure that all your questions and concerns are addressed.</p>
+                                        <h3 style="font-weight: 700; font-size: 16px;">Results</h3>
+                                        <p style="font-size: 14px;">Our tailored made strategies have a proven track record showing that we continuously deliver on our promise to maximize your returns.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pxp-carousel-controls">
+                        <a class="pxp-carousel-control-prev" role="button" data-slide="prev">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32.414" height="20.828" viewBox="0 0 32.414 20.828">
+                                <g id="Group_30" data-name="Group 30" transform="translate(-1845.086 -1586.086)">
+                                    <line id="Line_2" data-name="Line 2" x1="30" transform="translate(1846.5 1596.5)" fill="none" stroke="#333" stroke-linecap="round" stroke-width="2"/>
+                                    <line id="Line_3" data-name="Line 3" x1="9" y2="9" transform="translate(1846.5 1587.5)" fill="none" stroke="#333" stroke-linecap="round" stroke-width="2"/>
+                                    <line id="Line_4" data-name="Line 4" x1="9" y1="9" transform="translate(1846.5 1596.5)" fill="none" stroke="#333" stroke-linecap="round" stroke-width="2"/>
+                                </g>
+                            </svg>
+                        </a>
+                        <a class="pxp-carousel-control-next" role="button" data-slide="next">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32.414" height="20.828" viewBox="0 0 32.414 20.828">
+                                <g id="Symbol_1_1" data-name="Symbol 1 – 1" transform="translate(-1847.5 -1589.086)">
+                                    <line id="Line_5" data-name="Line 2" x2="30" transform="translate(1848.5 1599.5)" fill="none" stroke="#333" stroke-linecap="round" stroke-width="2"/>
+                                    <line id="Line_6" data-name="Line 3" x2="9" y2="9" transform="translate(1869.5 1590.5)" fill="none" stroke="#333" stroke-linecap="round" stroke-width="2"/>
+                                    <line id="Line_7" data-name="Line 4" y1="9" x2="9" transform="translate(1869.5 1599.5)" fill="none" stroke="#333" stroke-linecap="round" stroke-width="2"/>
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="pxp-carousel-ticker">
+                        <div class="pxp-carousel-ticker-counter"></div>
+                        <div class="pxp-carousel-ticker-divider">&nbsp;&nbsp;/&nbsp;&nbsp;</div>
+                        <div class="pxp-carousel-ticker-total"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php get_footer(); ?>
