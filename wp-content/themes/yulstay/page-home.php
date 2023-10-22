9267d9943f5d5c1a7dca232ba212ace7182c151f
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-10-22 08:25:12 +0530

    Test

--- wp-content/themes/yulstay/page-home.php
+++ wp-content/themes/yulstay/page-home.php
@@ -199,65 +199,6 @@ if ($lang == 'en-US'){
 
             ?>
 
-        <div class="container mt-100">
-            <h2 class="pxp-section-h2"><?php echo $home_team_section_title; ?></h2>
-            <p class="pxp-text-light"><?php echo $home_team_section_description; ?></p>
-
-            <div class="row mt-4 mt-md-5">
-                <?php $the_query = new WP_Query( array('post_type' =>'team','posts_per_page' => '4',  'post__not_in'   => array( $id),) );?>
-                <?php if ( have_posts())   : while ( $the_query->have_posts() ) : $the_query->the_post();?>
-
-                <?php
-										$thumbnail_id = get_post_thumbnail_id();
-										$thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true );
-										$thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true);
-
-                    $categories = get_the_category();
-
-										?>
-                <div class="col-sm-12 col-md-6 col-lg-3">
-                    <a href="<?php the_permalink(); ?>" class="pxp-agents-1-item">
-                        <?php if ( has_post_thumbnail() ) {
-                                $attachment_image = wp_get_attachment_url( get_post_thumbnail_id() );
-                                //echo '<link rel="preload" as="image" href="' . esc_attr( $attachment_image ) . '">';
-                            ?>
-                        <div class="pxp-agents-1-item-fig-container rounded-lg">
-                            <div class="pxp-agents-1-item-fig pxp-cover"
-                                style="background-image: url(<?php echo $attachment_image; ?>); background-position: top center">
-                            </div>
-                        </div>
-                        <?php } ?>
-                        <div class="pxp-agents-1-item-details rounded-lg">
-                            <div class="pxp-agents-1-item-details-name"><?php the_title(); ?></div>
-                            <div class="pxp-agents-1-item-details-email"><span class="fa fa-phone"></span>
-                                <?php echo get_field('team_member_telephone'); ?></div>
-                            <div class="pxp-agents-1-item-details-spacer"></div>
-                            <div class="pxp-agents-1-item-cta text-uppercase"><?php _e('More Details','theme-text-domain'); ?></div>
-                        </div>
-                        <div class="pxp-agents-1-item-rating"><span><span class="fa fa-star"></span><span
-                                    class="fa fa-star"></span><span class="fa fa-star"></span><span
-                                    class="fa fa-star"></span><span class="fa fa-star"></span></span></div>
-                    </a>
-                </div>
-                <?php endwhile; endif; ?>
-                <!-- <div class="col-sm-12 col-md-6 col-lg-3">
-                        <a href="single-agent.html" class="pxp-agents-1-item">
-                            <div class="pxp-agents-1-item-fig-container rounded-lg">
-                                <div class="pxp-agents-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-agent.jpg); background-position: top center"></div>
-                            </div>
-                            <div class="pxp-agents-1-item-details rounded-lg">
-                                <div class="pxp-agents-1-item-details-name">Alayna Becker</div>
-                                <div class="pxp-agents-1-item-details-email"><span class="fa fa-phone"></span> (456) 123-7890</div>
-                                <div class="pxp-agents-1-item-details-spacer"></div>
-                                <div class="pxp-agents-1-item-cta text-uppercase">More Details</div>
-                            </div>
-                            <div class="pxp-agents-1-item-rating"><span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star-o"></span></span></div>
-                        </a>
-                    </div> -->
-            </div>
-
-            <a href="<?php bloginfo('url'); ?>/team" class="pxp-primary-cta text-uppercase mt-1 mt-md-4 pxp-animate"><?php _e('See All Agents','theme-text-domain'); ?></a>
-        </div>
 
         <div class="pxp-testim-1 pt-100 pb-100 mt-100 pxp-cover" style="background-image: url(images/ph-big.jpg);">
                 <div class="pxp-testim-1-intro">
