6fccc7caf2000e19a11c7c2ea1db834d442c940c
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-10-23 21:49:44 +0530

    Contact 1

--- wp-content/themes/yulstay/page-contact.php
+++ wp-content/themes/yulstay/page-contact.php
@@ -54,9 +54,8 @@ $contact_page_email_address_link = get_field('contact_page_email_address_link');
 
                         <div class="container">
                             <div class="pxp-contact-hero-offices">
-                                
+                                <h2 class="pxp-section-h2"><?php echo $contact_page_contact_details_title; ?></h2>
                                 <div class="row">
-                                        <h2 class="pxp-section-h2"><?php echo $contact_page_contact_details_title; ?></h2>
                                     <div class="col-sm-12 col-md-6">
                                         <div class="pxp-contact-hero-offices-title"><?php echo $contact_page_address_line_one; ?></div>
                                         <div class="pxp-contact-hero-offices-info mt-2 mt-md-3">
