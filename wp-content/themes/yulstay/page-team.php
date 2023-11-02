b15e7c1fdfd15b0cb6fca208df0e525077dbd9a4
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-11-02 17:18:25 +0530

    Test

--- wp-content/themes/yulstay/page-team.php
+++ wp-content/themes/yulstay/page-team.php
@@ -88,7 +88,7 @@ $team_page_title_description = get_field('team_page_title_description');
                                 <?php } ?>
                                 <div class="pxp-agents-1-item-details rounded-lg">
                                     <div class="pxp-agents-1-item-details-name"><?php the_title(); ?></div>
-                                    <div class="pxp-agents-1-item-details-email"><?php echo $post_id; ?><span class="fa fa-phone"></span> <?php echo get_field('team_member_telephone'); ?></div>
+                                    <div class="pxp-agents-1-item-details-email"><?php if ($post_id == 32) {echo "no"; } else { echo '<span class="fa fa-phone"></span>'; } ?> <?php echo get_field('team_member_telephone'); ?></div>
                                     <div class="pxp-agents-1-item-details-spacer"></div>
                                     <div class="pxp-agents-1-item-cta text-uppercase"><?php _e('More Details','theme-text-domain'); ?></div>
                                 </div>
