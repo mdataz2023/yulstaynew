e6901b0acb252f85a3c6d64f685d2497433075fe
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-11-07 19:21:23 +0530

    Test

--- wp-content/themes/yulstay/header-home.php
+++ wp-content/themes/yulstay/header-home.php
@@ -1,3 +1,18 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+
 <!doctype html>
 
 
