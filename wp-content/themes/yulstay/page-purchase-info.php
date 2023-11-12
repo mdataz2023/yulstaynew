ddc97ec834aa04d9a47aa2fb30f92b92d44fe5ce
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-11-12 15:50:03 +0530

    Test

--- wp-content/themes/yulstay/page-purchase-info.php
+++ wp-content/themes/yulstay/page-purchase-info.php
@@ -4,95 +4,38 @@
 */
 get_header();
 
+$purchase_info_page_title = get_field('purchase_info_page_title');
+$purchase_info_page_description = get_field('purchase_info_page_description');
+$purchase_info_page_section_one_image = get_field('purchase_info_page_section_one_image');
+$purchase_info_page_section_one_title = get_field('purchase_info_page_section_one_title');
+$purchase_info_page_section_one_description = get_field('purchase_info_page_section_one_description');
+$purchase_info_page_section_two_image = get_field('purchase_info_page_section_two_image');
+$purchase_info_page_section_two_title = get_field('purchase_info_page_section_two_title');
+$purchase_info_page_section_two_description = get_field('purchase_info_page_section_two_description');
+$purchase_info_page_section_three_image = get_field('purchase_info_page_section_three_image');
+$purchase_info_page_section_three_title = get_field('purchase_info_page_section_three_title');
+$purchase_info_page_section_three_description = get_field('purchase_info_page_section_three_description');
+$purchase_info_page_section_four_image = get_field('purchase_info_page_section_four_image');
+$purchase_info_page_section_four_title = get_field('purchase_info_page_section_four_title');
+$purchase_info_page_section_four_description = get_field('purchase_info_page_section_four_description');
+$purchase_info_page_section_five_title = get_field('purchase_info_page_section_five_title');
+$purchase_info_page_section_five_description = get_field('purchase_info_page_section_five_description');
+
 ?>
 
         <div class="pxp-content">
             <div class="pxp-services-h pt-100 pb-100">
                 <div class="container">
-                    <h2 class="pxp-section-h2">Why Choose Us</h2>
-                    <p class="pxp-text-light">We offer perfect real estate services</p>
+                    <h2 class="pxp-section-h2"><?php echo $purchase_info_page_title; ?></h2>
+                    <p class="pxp-text-light" style="width: 45%;"><?php echo $purchase_info_page_description; ?></p>
 
                     <div class="pxp-services-h-container mt-4 mt-md-5">
-                        <div class="pxp-services-h-fig pxp-cover pxp-animate-in rounded-lg" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-big.jpg);"></div>
+                        <div class="pxp-services-h-fig pxp-cover pxp-animate-in rounded-lg" style="background-image: url(<?php echo $purchase_info_page_section_one_image; ?>);"></div>
                         <div class="pxp-services-h-items pxp-animate-in ml-0 ml-lg-5 mt-4 mt-md-5 mt-lg-0">
-                            <div class="pxp-services-h-item">
-                                <div class="media">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-1-1.svg" class="mr-4" alt="...">
-                                    <div class="media-body">
-                                        <h5 class="mt-0">Find your future home</h5>
-                                        We help you find a new home by offering a smart real estate experience
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="pxp-services-h-item mt-3 mt-md-4">
-                                <div class="media">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-2-1.svg" class="mr-4" alt="...">
-                                    <div class="media-body">
-                                        <h5 class="mt-0">Experienced agents</h5>
-                                        Find an agent who knows your market best
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="pxp-services-h-item mt-3 mt-md-4">
-                                <div class="media">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-3-1.svg" class="mr-4" alt="...">
-                                    <div class="media-body">
-                                        <h5 class="mt-0">Buy or rent homes</h5>
-                                        Millions of houses and apartments in your favourite cities
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="pxp-services-h-item mt-3 mt-md-4">
-                                <div class="media">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/service-icon-4-1.svg" class="mr-4" alt="...">
-                                    <div class="media-body">
-                                        <h5 class="mt-0">List your own property</h5>
-                                        Sign up now and sell or rent your own properties
-                                    </div>
-                                </div>
-                            </div>
-                            <a href="contact.html" class="pxp-primary-cta text-uppercase mt-4 mt-md-5 pxp-animate pxp-animate-in">Learn more</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        <h2 class="pxp-section-h2"><?php echo $purchase_info_page_section_one_title; ?></h2>
+                        <p class="pxp-is-address" style="padding-top: 12px;"><?php echo $purchase_info_page_section_one_description; ?></p>
 
-            <div class="pxp-services-columns mt-100">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <h2 class="pxp-section-h2 text-center">Your modern real estate platform</h2>
-                        </div>
-                        <div class="col-12">
-                            <div class="row mt-4 mt-md-5">
-                                <div class="col-md-4">
-                                    <div class="pxp-services-columns-item text-center mb-3 mb-md-4">
-                                        <div class="pxp-services-columns-item-fig">
-                                            <img src="<?php echo get_template_directory_uri(); ?>/images/d11-home-service-1-icon-200x200.png" alt="Find your future home">
-                                        </div>
-                                        <h3 class="mt-3">Find your future home</h3>
-                                        <p class="pxp-text-light">We help you find a new home by offering a smart real estate experience</p>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="pxp-services-columns-item text-center mb-3 mb-md-4">
-                                        <div class="pxp-services-columns-item-fig">
-                                            <img src="<?php echo get_template_directory_uri(); ?>/images/d11-home-service-2-icon-200x200.png" alt="Experienced agents">
-                                        </div>
-                                        <h3 class="mt-3">Experienced agents</h3>
-                                        <p class="pxp-text-light">Find an experienced agent who knows your market best</p>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="pxp-services-columns-item text-center mb-3 mb-md-4">
-                                        <div class="pxp-services-columns-item-fig">
-                                            <img src="<?php echo get_template_directory_uri(); ?>/images/d11-home-service-3-icon-200x200.png" alt="List your own property">
-                                        </div>
-                                        <h3 class="mt-3">List your own property</h3>
-                                        <p class="pxp-text-light">Sign up now and sell or rent your own properties</p>
-                                    </div>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
@@ -102,12 +45,12 @@ get_header();
                 <div class="container">
                     <div class="row align-items-center">
                         <div class="col-lg-5">
-                            <div class="pxp-cta-3-image pxp-cover rounded-lg" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-big.jpg);"></div>
+                            <div class="pxp-cta-3-image pxp-cover rounded-lg" style="background-image: url(<?php echo $purchase_info_page_section_two_image; ?>);"></div>
                         </div>
                         <div class="col-lg-2"></div>
                         <div class="col-lg-4">
-                            <h2 class="pxp-section-h2 mt-3 mt-md-5 mt-lg-0">Resideo concierge</h2>
-                            <p class="pxp-text-light mt-3 mt-lg-4">Concierge helps you sell your home faster and for more money by covering the cost of home improvement services — no upfront fees or interest charged.</p>
+                            <h2 class="pxp-section-h2 mt-3 mt-md-5 mt-lg-0"><?php echo $purchase_info_page_section_two_title; ?></h2>
+                            <p class="pxp-text-light mt-3 mt-lg-4"><?php echo $purchase_info_page_section_two_description; ?></p>
                             <a href="single-post.html" class="pxp-primary-cta text-uppercase mt-3 mt-md-5 pxp-animate">Learn more</a>
                         </div>
                         <div class="col-lg-1"></div>
@@ -120,13 +63,30 @@ get_header();
                     <div class="row align-items-center">
                         <div class="col-lg-1"></div>
                         <div class="col-lg-4">
-                            <h2 class="pxp-section-h2 mt-3 mt-md-5 mt-lg-0">A tech company reinventing the space</h2>
-                            <p class="pxp-text-light mt-3 mt-lg-4">To lead the industry requires the smartest tools built by the brightest minds across engineering, design, and strategy.</p>
+                            <h2 class="pxp-section-h2 mt-3 mt-md-5 mt-lg-0"><?php echo $purchase_info_page_section_three_description; ?></h2>
+                            <p class="pxp-text-light mt-3 mt-lg-4"><?php echo $purchase_info_page_section_three_description; ?></p>
                             <a href="single-post.html" class="pxp-primary-cta text-uppercase mt-3 mt-md-5 pxp-animate">Learn more</a>
                         </div>
                         <div class="col-lg-2"></div>
                         <div class="col-lg-5 order-first order-lg-last">
-                            <div class="pxp-cta-3-image pxp-cover rounded-lg" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-big.jpg);"></div>
+                            <div class="pxp-cta-3-image pxp-cover rounded-lg" style="background-image: url(<?php echo $purchase_info_page_section_three_image; ?>);"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pxp-cta-3 mt-100">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-1"></div>
+                        <div class="col-lg-4">
+                            <h2 class="pxp-section-h2 mt-3 mt-md-5 mt-lg-0"><?php echo $purchase_info_page_section_four_title; ?></h2>
+                            <p class="pxp-text-light mt-3 mt-lg-4"><?php echo $purchase_info_page_section_four_description; ?></p>
+                            <a href="single-post.html" class="pxp-primary-cta text-uppercase mt-3 mt-md-5 pxp-animate">Learn more</a>
+                        </div>
+                        <div class="col-lg-2"></div>
+                        <div class="col-lg-5 order-first order-lg-last">
+                            <div class="pxp-cta-3-image pxp-cover rounded-lg" style="background-image: url(<?php echo $purchase_info_page_section_four_image; ?>);"></div>
                         </div>
                     </div>
                 </div>
@@ -137,8 +97,8 @@ get_header();
                     <div class="row justify-content-center">
                         <div class="col-md-8 col-lg-6">
                             <div class="text-center">
-                                <h2 class="pxp-section-h2">A real estate company with a purpose.</h2>
-                                <p class="pxp-text-light mt-3 mt-lg-4">Resideo is building the first modern real estate platform, pairing the industry’s top talent with technology to make the search and sell experience intelligent and seamless.</p>
+                                <h2 class="pxp-section-h2"><?php echo $purchase_info_page_section_five_title; ?></h2>
+                                <p class="pxp-text-light mt-3 mt-lg-4"><?php echo $purchase_info_page_section_five_description; ?></p>
                                 <a href="contact.html" class="pxp-primary-cta text-uppercase mt-3 mt-md-5 pxp-animate">Contact us now</a>
                             </div>
                         </div>
