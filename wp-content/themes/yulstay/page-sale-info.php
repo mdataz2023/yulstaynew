6fc42be1bcdb9db91ef6adc7676b334f6c6d06db
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-11-19 19:34:08 +0530

    Test

--- wp-content/themes/yulstay/page-sale-info.php
+++ wp-content/themes/yulstay/page-sale-info.php
@@ -28,7 +28,7 @@ $sale_info_page_section_four_description = get_field('sale_info_page_section_fou
                 <div class="container">
                     <h2 class="pxp-section-h2"><?php echo $sale_info_page_title; ?></h2>
                     <p class="pxp-text-light info-des"><?php echo $sale_info_page_description; ?></p>
-                    <a href="<?php bloginfo('url'); ?>/sale-form" class="pxp-primary-cta text-uppercase pxp-animate"><span class="fa fa-phone"></span> <?php _e('sale info Form','theme-text-domain'); ?></a>
+                    <a href="<?php bloginfo('url'); ?>/sale-form" class="pxp-primary-cta text-uppercase pxp-animate"><span class="fa fa-phone"></span> <?php _e('sale Team','theme-text-domain'); ?></a>
 
                     <div class="pxp-services-h-container mt-4 mt-md-5">
                         <div class="pxp-services-h-fig pxp-cover pxp-animate-in rounded-lg" style="background-image: url(<?php echo $sale_info_page_section_one_image; ?>);"></div>
