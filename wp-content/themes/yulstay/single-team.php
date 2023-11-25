aa2f82b62a74236c48e56a932e620311d6a15790
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-11-25 21:49:44 +0530

    Test

--- wp-content/themes/yulstay/single-team.php
+++ wp-content/themes/yulstay/single-team.php
@@ -42,15 +42,15 @@ $post_id = get_the_ID();
                     <div class="mt-4 mt-md-5">
                         <div class="pxp-agent-email" style="display: flex; gap: 8px; align-items: center; margin-bottom: 10px;">
                             <div class="fa fa-envelope-o"></div>
-                            <a href="mailto:<?php echo $team_member_email; ?>"><?php echo $team_member_email; ?></a>
+                            <a href="mailto:<?php echo $team_member_email; ?>" style=" margin: 0px !important;"><?php echo $team_member_email; ?></a>
                         </div>
                         <div class="pxp-agent-email" style="display: flex; gap: 8px; align-items: center; margin-bottom: 10px;">
                             <div class="fa fa-location-arrow"></div>
-                            <a href=""><?php echo $team_member_address; ?></a>
+                            <a href="" style=" margin: 0px !important;"><?php echo $team_member_address; ?></a>
                         </div>
                         <div class="pxp-agent-phone" style="display: flex; gap: 8px; align-items: center; margin-bottom: 10px;">
                             <div><?php if ($post_id == 32) {echo " "; } else { echo '<div class="fa fa-phone"></div>'; } ?></div>
-                            <a href="tel:<?php echo $team_member_telephone; ?>" style="color: #333 !important;"><?php echo $team_member_telephone; ?></a>
+                            <a href="tel:<?php echo $team_member_telephone; ?>" style="color: #333 !important; margin: 0px !important;"><?php echo $team_member_telephone; ?></a>
                         </div>
                     </div>
                     <div hidden class="mt-4 mt-md-5">
