3f072a88f5636592bbefaa37dfd9f8920ee04e9e
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-12-13 16:57:10 +0530

    Test

--- wp-content/themes/yulstay/single-team.php
+++ wp-content/themes/yulstay/single-team.php
@@ -71,7 +71,7 @@ $post_id = get_the_ID();
             <div class="row">
                 <div class="col-sm-12 col-lg-8">
                     <div class="pxp-agent-section mt-4 mt-md-5">
-                        <h3>About <?php echo $team_member_name; ?></h3>
+                        <h3><?php _e('About','theme-text-domain'); ?> <?php echo $team_member_name; ?></h3>
                         <div class="mt-3 mt-md-4">
                             <?php if( have_rows('team_member_paragraphs') ): $s= 1; $a=0; ?>
 
