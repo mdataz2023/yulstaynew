99c831799dc991e2f9c4d16417d51c91dc14b971
Nikalanda Bandara <chandima96dilan@gmail.com>
2024-03-13 18:47:07 +0530

    Podcast

---
+++ wp-content/themes/yulstay/page.podcast.php
@@ -0,0 +1,189 @@
+<?php
+/*
+	Template Name: Podcast
+*/
+get_header();
+
+?>
+
+        <div class="pxp-content">
+            <div class="pxp-blog-posts pxp-content-wrapper mt-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-7">
+                            <h1 class="pxp-page-header">Podcast</h1>
+                            <p class="pxp-text-light">Say hello. Tell us how we can guide you.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="pxp-contact-hero mt-4 mt-md-5">
+                    <div class="pxp-contact-hero-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/imageres1.png); background-position: 50% 80%;"></div>
+                </div>
+
+                <div class="container">
+
+                    <div class="row mt-100">
+                            <div class="row">
+                                <?php $the_query = new WP_Query( array('post_type' =>'podcast','posts_per_page' => '6',  'post__not_in'   => array( $id),) );?>
+							     <?php if ( have_posts())   : while ( $the_query->have_posts() ) : $the_query->the_post();?>
+							  
+									   <?php
+										$thumbnail_id = get_post_thumbnail_id(); 
+										$thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true );
+										$thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true); 
+
+                                $categories = get_the_category();
+										
+								?>
+
+                                <div class="col-sm-12 col-md-4">
+                                    <a href="<?php the_permalink(); ?>" class="pxp-posts-1-item">
+                                        <?php 
+                                        if ( has_post_thumbnail() ) {
+                                            $attachment_image = wp_get_attachment_url( get_post_thumbnail_id() );
+                                        ?>
+                                        <div class="pxp-posts-1-item-fig-container">
+                                            <div class="pxp-posts-1-item-fig pxp-cover" style="background-image: url(<?php echo $attachment_image; ?>);"></div>
+                                        </div>
+                                        <?php } ?>
+                                        <div class="pxp-posts-1-item-details">
+                                            <div class="pxp-posts-1-item-details-category">Interior Design</div>
+                                            <div class="pxp-posts-1-item-details-title"><?php the_title(); ?></div>
+                                            <div class="pxp-posts-1-item-details-date mt-2">April 9, 2024</div>
+                                            <div class="pxp-posts-1-item-cta text-uppercase">Read Article</div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <?php endwhile; endif; wp_reset_Query(); ?>
+
+                                <div class="col-sm-12 col-md-4">
+                                    <a href="single-post.html" class="pxp-posts-1-item">
+                                        <div class="pxp-posts-1-item-fig-container">
+                                            <div class="pxp-posts-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/team.jpg);"></div>
+                                        </div>
+                                        <div class="pxp-posts-1-item-details">
+                                            <div class="pxp-posts-1-item-details-category">Architecture</div>
+                                            <div class="pxp-posts-1-item-details-title">Private Contemporary Home Balancing Openness</div>
+                                            <div class="pxp-posts-1-item-details-date mt-2">April 9, 2021</div>
+                                            <div class="pxp-posts-1-item-cta text-uppercase">Read Article</div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-sm-12 col-md-4">
+                                    <a href="single-post.html" class="pxp-posts-1-item">
+                                        <div class="pxp-posts-1-item-fig-container">
+                                            <div class="pxp-posts-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/team.jpg);"></div>
+                                        </div>
+                                        <div class="pxp-posts-1-item-details">
+                                            <div class="pxp-posts-1-item-details-category">Interior Design</div>
+                                            <div class="pxp-posts-1-item-details-title">Stylish Modern Ranch Exuding a Welcoming Feel</div>
+                                            <div class="pxp-posts-1-item-details-date mt-2">April 9, 2021</div>
+                                            <div class="pxp-posts-1-item-cta text-uppercase">Read Article</div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-sm-12 col-md-4">
+                                    <a href="single-post.html" class="pxp-posts-1-item">
+                                        <div class="pxp-posts-1-item-fig-container">
+                                            <div class="pxp-posts-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/team.jpg);"></div>
+                                        </div>
+                                        <div class="pxp-posts-1-item-details">
+                                            <div class="pxp-posts-1-item-details-category">Interior Design</div>
+                                            <div class="pxp-posts-1-item-details-title">What to Expect When Working with an Interior Designer</div>
+                                            <div class="pxp-posts-1-item-details-date mt-2">April 9, 2021</div>
+                                            <div class="pxp-posts-1-item-cta text-uppercase">Read Article</div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-sm-12 col-md-4">
+                                    <a href="single-post.html" class="pxp-posts-1-item">
+                                        <div class="pxp-posts-1-item-fig-container">
+                                            <div class="pxp-posts-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/team.jpg);"></div>
+                                        </div>
+                                        <div class="pxp-posts-1-item-details">
+                                            <div class="pxp-posts-1-item-details-category">Architecture</div>
+                                            <div class="pxp-posts-1-item-details-title">Private Contemporary Home Balancing Openness</div>
+                                            <div class="pxp-posts-1-item-details-date mt-2">April 9, 2021</div>
+                                            <div class="pxp-posts-1-item-cta text-uppercase">Read Article</div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-sm-12 col-md-4">
+                                    <a href="single-post.html" class="pxp-posts-1-item">
+                                        <div class="pxp-posts-1-item-fig-container">
+                                            <div class="pxp-posts-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/team.jpg);"></div>
+                                        </div>
+                                        <div class="pxp-posts-1-item-details">
+                                            <div class="pxp-posts-1-item-details-category">Interior Design</div>
+                                            <div class="pxp-posts-1-item-details-title">Stylish Modern Ranch Exuding a Welcoming Feel</div>
+                                            <div class="pxp-posts-1-item-details-date mt-2">April 9, 2021</div>
+                                            <div class="pxp-posts-1-item-cta text-uppercase">Read Article</div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <ul class="pagination pxp-paginantion mt-3 mt-md-4">
+                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">Next <span class="fa fa-angle-right"></span></a></li>
+                            </ul>
+                        </div>
+                        <!-- <div class="col-sm-12 col-lg-3 mt-4 mt-md-5 mt-lg-0">
+                            <div class="pxp-blog-posts-side-section">
+                                <h3>Search Articles</h3>
+                                <div class="mt-3 mt-md-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control pxp-is-address" placeholder="Search">
+                                        <span class="fa fa-search"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="pxp-blog-posts-side-section mt-4 mt-md-5">
+                                <h3>Categories</h3>
+                                <ul class="pxp-blog-posts-side-v-list list-unstyled mt-3 mt-md-4">
+                                    <li><a href="#">Fashion (3)</a></li>
+                                    <li><a href="#">Lifestyle (2)</a></li>
+                                    <li><a href="#">Personal (2)</a></li>
+                                    <li><a href="#">Stories (2)</a></li>
+                                    <li><a href="#">Travel (4)</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="pxp-blog-posts-side-section mt-4 mt-md-5">
+                                <h3>Tags</h3>
+                                <div class="pxp-blog-posts-side-tags mt-3 mt-md-4">
+                                    <a href="#">Premium (10)</a>
+                                    <a href="#">Interior (12)</a>
+                                    <a href="#">Stories (6)</a>
+                                    <a href="#">Fashion (2)</a>
+                                    <a href="#">Architecture (8)</a>
+                                    <a href="#">Lifestyle (5)</a>
+                                    <a href="#">Travel (10)</a>
+                                    <a href="#">Personal (11)</a>
+                                </div>
+                            </div>
+                        </div> -->
+                </div>
+            </div>
+        </div>
+
+        <div class="pxp-full pxp-cover pt-100 pb-100 mt-100" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/imageres3.png);">
+            <div class="container">
+                <h2 class="pxp-section-h2">Stay Up to Date</h2>
+                <p class="pxp-text-light">Subscribe to our newsletter to receive our weekly feed</p>
+                <div class="row mt-4 mt-md-5">
+                    <div class="col-xs-12 col-sm-6">
+                        <form action="blog.html" class="pxp-newsletter-1-form">
+                            <input type="text" class="form-control" placeholder="Enter your email...">
+                            <a href="#" class="pxp-primary-cta text-uppercase pxp-animate mt-3 mt-md-4">Subscribe</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/yulstay/single-podcast.php
@@ -0,0 +1,130 @@
+<?php
+/*
+	Template Name: Join Our Team
+*/
+get_header();
+
+?>
+
+        <div class="pxp-content">
+            <div class="pxp-blog-posts pxp-content-wrapper mt-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-9 col-lg-7">
+                            <div class="pxp-blog-post-category"><span>April 9, 2021</span><span>Interior Design</span></div>
+                            <h1 class="pxp-page-header">What to Expect When Working with an Interior Designer</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="pxp-blog-post-hero mt-4 mt-md-5">
+                    <div class="pxp-blog-post-hero-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/team.jpg); background-position: 50% 50%;"></div>
+                </div>
+
+                <div class="container mt-100">
+                    <div class="row">
+                        <div class="col-sm-12 col-lg-1">
+                            <div class="pxp-blog-post-share">
+                                <div class="pxp-blog-post-share-label">Share</div>
+                                <ul class="list-unstyled mt-3">
+                                    <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                                    <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                                    <li><a href="#"><span class="fa fa-pinterest"></span></a></li>
+                                    <li><a href="#"><span class="fa fa-linkedin"></span></a></li>
+                                </ul>
+                                <div class="clearfix"></div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-10">
+                            <div class="pxp-blog-post-block mt-4 mt-md-5 mt-lg-0">
+                                <h2>Have realistic expectations</h2>
+                                <div class="mt-3 mt-md-4">
+                                    <p class="pxp-first-letter">Most designers will tell you that, as much as we all love to watch home design shows, their prevalence has done them a bit of a disservice. Thanks to TV magic, the designers on those shows pull off projects with tight deadlines and shoestring budgets that would never fly in the real world.</p>
+                                    <p>Of course, every project is different. The best way to get a handle on an anticipated budget and duration is by asking potential designers for this information upfront. Don’t be afraid to reach out to more than one to get a realistic picture of what to expect.</p>
+                                </div>
+                            </div>
+
+                            <div class="pxp-blog-post-block pxp-full mt-4 mt-md-5">
+                                <a href="javascript:void(0);" class="pxp-blog-post-video pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/team.jpg); background-position: 50% 50%;"></a>
+                                <iframe width="889" height="500" src="https://www.youtube.com/embed/4Wee4LASqvE?start=23" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width: 100%; border: 0 none;"></iframe>
+                            </div>
+
+                            <!-- <div class="pxp-blog-post-block mt-4 mt-md-5">
+                                <h2>Take the time to find the right fit</h2>
+                                <div class="mt-3 mt-md-4">
+                                    <p>Not all designers are created equal. Like all of us, each designer has his or her own unique personal tastes, quirks and business practices. To make sure your project is a success, it’s important that you and your designer are on the same page with a lot of these details. You may need to interview a few before you find someone who’s the right fit.</p>
+                                    <div class="pxp-blog-post-blockquote pxp-left">"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."</div>
+                                    <p>In the design world, this interview is called a consultation. It can be an in-person meeting or held over the phone, and it can be paid or unpaid. You can use this time to ask to see samples of the designer’s work, learn more about their process, and ask about business practices, such as their preferred methods for communication and billing.</p>
+                                    <p>You can also use this time to let the designer get to know you. Feel free to bring in a few photos or items you intend to use for design inspiration. Let the designer know about your specific quirks and personal preferences. By the end of the meeting, you should have a good sense of whether the two of you will work well together.</p>
+                                </div>
+                            </div> -->
+
+                            <!-- <div class="pxp-blog-post-block pxp-full mt-4 mt-md-5">
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-6">
+                                        <img src="images/ph-gallery.jpg" alt="" class="pxp-image-full mb-3">
+                                    </div>
+                                    <div class="col-sm-12 col-md-6">
+                                        <img src="images/ph-gallery.jpg" alt="" class="pxp-image-full mb-3">
+                                    </div>
+                                </div>
+                            </div> -->
+
+                            <!-- <div class="pxp-blog-post-block mt-4 mt-md-5">
+                                <h2>Final thoughts</h2>
+                                <div class="mt-3 mt-md-4">
+                                    <p>For those who have never hired an interior designer before, the idea of doing so can feel out of reach, but it doesn’t need to be. We’ve created a first-timer’s guide to working with an interior designer to help you take the plunge. Use the advice in this post to make an informed decision as to whether hiring professional help is the right choice for you.</p>
+                                </div>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container mt-100">
+                    <h2 class="pxp-section-h2">Related Articles</h2>
+                    <div class="row mt-4 mt-md-5">
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <a href="#" class="pxp-posts-1-item">
+                                <div class="pxp-posts-1-item-fig-container">
+                                    <div class="pxp-posts-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-gallery.jpg);"></div>
+                                </div>
+                                <div class="pxp-posts-1-item-details">
+                                    <div class="pxp-posts-1-item-details-category">Interior Design</div>
+                                    <div class="pxp-posts-1-item-details-title">What to Expect When Working with an Interior Designer</div>
+                                    <div class="pxp-posts-1-item-details-date mt-2">April 9, 2021</div>
+                                    <div class="pxp-posts-1-item-cta text-uppercase">Read Article</div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <a href="#" class="pxp-posts-1-item">
+                                <div class="pxp-posts-1-item-fig-container">
+                                    <div class="pxp-posts-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/ph-gallery.jpg);"></div>
+                                </div>
+                                <div class="pxp-posts-1-item-details">
+                                    <div class="pxp-posts-1-item-details-category">Architecture</div>
+                                    <div class="pxp-posts-1-item-details-title">Private Contemporary Home Balancing Openness</div>
+                                    <div class="pxp-posts-1-item-details-date mt-2">April 9, 2021</div>
+                                    <div class="pxp-posts-1-item-cta text-uppercase">Read Article</div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <a href="#" class="pxp-posts-1-item">
+                                <div class="pxp-posts-1-item-fig-container">
+                                    <div class="pxp-posts-1-item-fig pxp-cover" style="background-image: url(<?php echo get_template_directory_uri(); ?>/imagess/ph-gallery.jpg);"></div>
+                                </div>
+                                <div class="pxp-posts-1-item-details">
+                                    <div class="pxp-posts-1-item-details-category">Interior Design</div>
+                                    <div class="pxp-posts-1-item-details-title">Stylish Modern Ranch Exuding a Welcoming Feel</div>
+                                    <div class="pxp-posts-1-item-details-date mt-2">April 9, 2021</div>
+                                    <div class="pxp-posts-1-item-cta text-uppercase">Read Article</div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php get_footer(); ?>
